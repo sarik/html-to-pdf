a0236773f7181ccdaf1169610e9b2e53a92e2d90
Sarik Siddiqui <siddiquisarik@gmail.com>
2021-04-21 11:36:20 +0530

    added TODO markers

--- sdk.js
+++ sdk.js
@@ -4,12 +4,16 @@ const { exception } = require("console");
 const { exit } = require("process");
 
 async function genratePDf() {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
+  let browser;
+  let page;
   try {
+    browser = await puppeteer.launch();
+    page = await browser.newPage();
+    //TODO:Relative path to script
     var data = fs.readFileSync("sample.json", "utf8");
     var config = JSON.parse(data);
 
+    //TODO:Give absolute path beginning with file://
     await page.goto("file:///Users/user2/ws/express-pdf/report.html");
     //await page.goto("report.html");
     let content = await page.content();
@@ -23,19 +27,22 @@ async function genratePDf() {
     const buffer = await page.pdf({ format: "A4" });
     await browser.close();
     console.log(buffer);
+
+    //TODO:Relative path to generated pdf
     fs.writeFileSync("abcdef.pdf", buffer);
   } catch (e) {
+    console.log(e)
     await browser.close();
-    //throw new Error('Something bad happened'); 
-    await new Promise((res,rej) => setTimeout(() => res(""),3000))
+    //throw new Error('Something bad happened');
+    await new Promise((res, rej) => setTimeout(() => res(""), 3000));
     //Check if the shell command waits
     /*await new Promise((resolve, reject) => {
         setTimeout(() => {
           resolve('foo');
         }, 300);
       });*/
-    console.log("somethign")
-    process.exit(1)
+    console.log("somethign");
+    process.exit(1);
   }
 }
 
